57394bd798bfd7eaf95a9dce5b8009daf4302ee8
Lai Tuan <laituan245@kaist.ac.kr>
2016-06-07 17:23:02 +0900

    Re-added xgboost (got better rank) + Lb score = 1.02407 + train-mlogloss:0.717850+0.000964 + test-mlogloss:0.831790+0.003896

---
+++ xgboost.R
@@ -0,0 +1,36 @@
+set.seed(2405)
+library(xgboost)
+library(Matrix)
+
+train <- read.csv('preprocessed_data/train.csv', header = T)
+test <- read.csv('preprocessed_data/test.csv', header = T)
+train$ageuponoutcome <- factor(train$ageuponoutcome, levels=c(levels(train$ageuponoutcome), "22 years"))
+train$year <- as.factor(train$year)
+train$month <- as.factor(train$month)
+test$year <- as.factor(test$year)
+test$month <- as.factor(test$month)
+
+train_x <- sparse.model.matrix(outcometype~.-1, data = train)
+train_y <- as.integer(train$outcometype)-1
+test_x <- sparse.model.matrix(~.-1, data = test)
+
+# parameters
+param <- list("objective" = "multi:softprob",
+              "eval_metric" = "mlogloss",
+              "num_class" = 5,
+              "max_depth" = 5,
+              "eta"=0.1,
+              "subsample"=0.75,
+              "colsample_bytree"=0.85)
+
+# train the model
+nround = 300
+bst = xgboost(data = train_x, label = train_y, param=param, nrounds = nround)
+cv = xgb.cv(data = train_x, label = train_y, param=param, nrounds = nround, nfold = 5)
+
+# doing prediction
+ypred = predict(bst, test_x)
+m = as.data.frame( t ( matrix(ypred, ncol = 11456, nrow = 5) ) )
+colnames(m) = c("Adoption","Died","Euthanasia","Return_to_owner","Transfer")
+submission = cbind(ID=1:nrow(test), m)
+write.csv(submission, file="submission.csv", row.names=FALSE, quote=FALSE)
